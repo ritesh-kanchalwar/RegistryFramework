7748c865d34620a75cde732dcd93f7eda75dd958
Ritesh Ashok Kanchalwar <riteshashok.k@photon.com>
2022-04-12 15:49:44 +0530

    Chaged SwiftUI File - > Added List

--- Classes/FirstView/RegistryViewControllerSwiftUI.swift
+++ Classes/FirstView/RegistryViewControllerSwiftUI.swift
@@ -10,23 +10,45 @@ import SwiftUI
 public struct RegistryViewControllerSwiftUI: View {
     public init () {}
     public var body: some View {
-        VStack {
-            Spacer()
-            HStack {
-                Spacer()
-                Text("Registry App Comming Soon!!!!")
-                    .bold()
-                    .foregroundColor(.black)
-                Spacer()
+//        VStack {
+//            Spacer()
+//            HStack {
+//                Spacer()
+//                Text("Registry App Comming Soon!!!!")
+//                    .bold()
+//                    .foregroundColor(.black)
+//                Spacer()
+//            }
+//            Spacer()
+//        }.background(Color.gray)
+//        .edgesIgnoringSafeArea(.all)
+//    }
+        List {
+                ShowText(titleText: "Registry 1")
+                ShowText(titleText: "Registry 2")
+                ShowText(titleText: "Registry 3")
+                ShowText(titleText: "Registry 4")
+                ShowText(titleText: "Registry 5")
+                ShowText(titleText: "Registry 6")
+                ShowText(titleText: "Registry 7")
+                ShowText(titleText: "Registry 8")
+                ShowText(titleText: "Registry 9")
+                ShowText(titleText: "Registry 10")
             }
-            Spacer()
-        }.background(Color.gray)
-        .edgesIgnoringSafeArea(.all)
-    }
+        }
 }
 
 public struct RegistryViewControllerSwiftUI_Previews: PreviewProvider {
     public static var previews: some View {
         RegistryViewControllerSwiftUI()
+    }
+}
+
+struct ShowText: View {
+    var titleText:String = ""
+
+    var body: some View {
+        Text(titleText)
+            .fontWeight(.bold)
     }
 }
--- RegistryFramework.podspec
+++ RegistryFramework.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'RegistryFramework'
-  s.version          = '1.0.4'
+  s.version          = '1.0.5'
   s.summary          = 'This is a short description for RegistryFramework.'
 
 # This description is used to generate tags and improve search results.
